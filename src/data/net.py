a0f6e62758da3fc8ea6147e5d4cc045509f63db2
Claudio2ooo <riccioclaudio2000@gmail.com>
2024-12-22 11:31:54 +0100

    definita la rete neurale

--- src/data/net.py
+++ src/data/net.py
@@ -1,5 +1,16 @@
+import torch.nn as nn
+
 
 class Net(nn.Module):
     def __init__(self):
-        super(Net, self,).__init__()
-        self.function = nn
+        super(Net, self, ).__init__()
+        self.hidden_layer = nn.Linear(28 * 28, 128)
+        self.relu = nn.ReLU()
+        self.output_layer = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = self.hidden_layer(x)
+        x = self.relu(x)
+        x = self.output_layer(x)
+        return x
